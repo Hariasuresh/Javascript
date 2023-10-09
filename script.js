0392fd84b0b9cc23bf4a6292ea97298bc5d9776c
Hariasuresh <Hariasuresh1@gmail.com>
2023-10-09 21:43:43 +0530

    practiced

---
+++ script.js
@@ -0,0 +1,17 @@
+let Size = "S";
+if(Size==="XL")
+{
+    console.log(`price is rs 250`);
+}
+else if(Size==="L")
+{
+    console.log(`price is rs 200`);
+}
+else if(Size==="M")
+{
+    console.log(`price is rs 100`);
+}
+else if(Size==="S")
+{
+    console.log(`price is rs 50`);
+}
